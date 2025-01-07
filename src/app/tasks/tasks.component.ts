03cefe2197737a670fab8f0bbe13d73e086e85df
Tafadzwa-Matare <mataretafadzwa@gmail.com>
2025-01-07 23:40:24 +0200

    added new-task logic

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,19 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent]
+  imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
 
   
   @Input({required: true}) userId!: string;
   @Input({required: true}) name? : string; 
-
+  isAddingTask = false;
 
   tasks = [
     {
@@ -43,6 +44,18 @@ export class TasksComponent {
 
   get selectedUserTasks(){
     return this.tasks.filter((task) => task.userId === this.userId)
+  }
+
+  onCompleteTask(id: string){
+    this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
   }
 
 }
