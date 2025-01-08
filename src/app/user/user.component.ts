88a115d966c0780e559bd3ed71b3a823216eb22c
Tafadzwa-Matare <mataretafadzwa@gmail.com>
2025-01-08 11:03:57 +0200

    added content projection logic

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 // type User = {
 //   id: string;
 //   avatar: string;
@@ -10,7 +11,8 @@ import { User } from './user.model';
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
